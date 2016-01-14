ee8f5c2710eb14f193e34d21bf3c342eed9ba0a0
Jose Gomez-Dans <jgomezdans@gmail.com>
2016-01-14 12:23:18 +0000

    The main Sentinel2 downloader is working. Can't do parallel downloads due to the limit of two concurrent uses, so not bothering with the added complexity. The code appears to work OK, but the server seems to crash quite often, not sure whether this is related to my code or to the server. Worst case, might need to resort to wget to download things if the Python urllib2 implementation does not work...

--- grabba_grabba_hey/query_builder.py
+++ grabba_grabba_hey/query_builder.py
@@ -1,7 +1,11 @@
+import os
 import datetime
 import xml.etree.cElementTree as ET
 import urllib2
 import base64
+from multiprocessing.dummy import Pool 
+
+hub_url = "https://scihub.copernicus.eu/dhus/search?q="
 
 class PreemptiveBasicAuthHandler(urllib2.HTTPBasicAuthHandler):
     '''Preemptive basic auth.
@@ -23,58 +27,8 @@ class PreemptiveBasicAuthHandler(urllib2.HTTPBasicAuthHandler):
 
     https_request = http_request
 
-def parse_xml ( xmlfile ):
-    tree = ET.ElementTree( file = xmlfile )
-    # Search for all the acquired images...
-    for elem in tree.iter (tag="{http://www.w3.org/2005/Atom}entry"):
-        for img in elem.getchildren():
-            print img.tag
-            for x in img.getchildren():
-                print ">>>",x
-
-def run_query ( url, username="guest", password="guest" ):
-    
-
-    # Create an OpenerDirector with support for Basic HTTP Authentication...
-    auth_handler = urllib2.HTTPBasicAuthHandler()
-    auth_handler.add_password(None, "http://scihub.copernicus.eu/apihub/", 
-                              username, password)
-
-    opener = urllib2.build_opener(auth_handler)
-    print "Opening..."
-    return  opener.open(url).read()
-
-if __name__ == "__main__":
-
-    location = ( 42.8,-8.1)
-    start_date = datetime.datetime (2015, 1, 1)
-    end_date = None
-    sensor = "S2"
-    url = "https://scihub.copernicus.eu/apihub/search?q="
-    """    https://scihub.copernicus.eu/apihub/search?q=footprint:%22Intersects(42.800000,-8.100000)%22%20filename:S2*%20ingestiondate:[2015-01-01T00:00:00.000Z%20TO%20NOW]"""
-    if len(location) == 2:
-        location_str="footprint:\\Intersects(%f,%f)" % ( location[0], location[1] )
-    elif len(location) == 4:
-        location_str = "footprint:\\Intersects( ( POLYGON(( " + \
-            "%f %f, %f %f, %f %f, %f %f, %f %f) )" % (  
-        location[0], location[1],
-        location[0], location[1],
-        location[0], location[1],
-        location[0], location[1],
-        location[0], location[1] )
-    if sensor == "S1":
-        sensor_str ="filename:S1*"
-    elif sensor == "S2":
-        sensor_str ="filename:S2*"
-    time_str = "ingestiondate:[%sZ" % start_date.isoformat()
-    if end_date is None:
-        time_str = time_str + " TO NOW]"
-    query = "%s%s %s %s" % ( url, location_str, sensor_str, time_str )
-    query = "%s%s" % ( url, urllib2.quote(location_str) )
-    print query
-    api_url = "https://scihub.copernicus.eu/"
-    api_username = "guest"
-    api_password = "guest"
+def do_query ( query, api_username="guest", api_password="guest", 
+              api_url="https://scihub.copernicus.eu/" ):    
 
     auth_handler = PreemptiveBasicAuthHandler()
     auth_handler.add_password(
@@ -85,18 +39,155 @@ if __name__ == "__main__":
     opener = urllib2.build_opener(auth_handler)
     urllib2.install_opener(opener)
     result = urllib2.urlopen ( query ).read()
-    print result
+    return result
+
+def download ( x, api_username="guest", api_password="guest", 
+              api_url="https://scihub.copernicus.eu/" ):    
+    source, target = x
+    auth_handler = PreemptiveBasicAuthHandler()
+    auth_handler.add_password(
+        realm=None, # default realm.
+        uri=api_url,
+        user=api_username,
+        passwd=api_password)
+    opener = urllib2.build_opener(auth_handler)
+    urllib2.install_opener(opener)
+    print urllib2.quote(source, "/:" )
+    u = urllib2.urlopen ( urllib2.quote(source, "/:" ) )
+    fp = open( target, 'wb')
+    meta = u.info()
+    file_size = int(meta.getheaders("Content-Length")[0])
+    print "Downloading: %s Bytes: %s" % (target, file_size)
+    file_size_dl = 0
+    block_sz = 8192
+    while True:
+        buff = u.read(block_sz)
+        if not buff:
+            break
+        file_size_dl += len(buff)
+        fp.write(buff)
+        status = r"%10d  [%3.2f%%]" % (file_size_dl, file_size_dl * 100. / file_size)
+        status = status + chr(8)*(len(status)+1)
+        print status,
+
+    fp.close()
+    return result
+
+
+def parse_xml ( xml ):
+    fields_of_interest = [ "filename", "identifier", "instrumentshortname", 
+                          "orbitnumber", "orbitdirection", "producttype", 
+                          "beginposition", "endposition" ]
+    tree = ET.ElementTree( ET.fromstring(xml) )
+    # Search for all the acquired images...
+    granules = []
+    for elem in tree.iter (tag="{http://www.w3.org/2005/Atom}entry"):
+        granule = {}
+        for img in elem.getchildren():
+            if img.tag.find ("id") >= 0:
+                granule['id'] = img.text
+            if img.tag.find ("link") and img.attrib.has_key ( "href" ):
+                
+                if img.attrib['href'].find ("Quicklook") >= 0:
+                    granule['quicklook'] = img.attrib['href']
+                elif img.attrib['href'].find ("$value") >= 0:
+                    granule['link'] = img.attrib['href'].replace("$value", "" )
+                    
+            if img.attrib.has_key("name"):
+                if img.attrib['name'] in fields_of_interest:
+                    granule[img.attrib['name']] = img.text
+        
+        granules.append ( granule )
+    
+    return granules
+            #print img.tag, img.attrib, img.text
+            #for x in img.getchildren():
+                
+
+def download_sentinel ( location, input_start_date, input_sensor, output_dir,
+                       input_end_date=None, username="guest", password="guest" ):
+
+
+
+    
+    input_sensor = input_sensor.upper()
+    sensor_list = ["S1", "S2" ]
+    if not input_sensor in sensor_list:
+        raise ValueError("Sensor can only be S1 or S2. You provided %s" 
+                         % input_sensor )
+    else:
+        sensor_str = 'filename:%s*' % input_sensor.upper()
+    
+    try:
+        start_date = datetime.datetime.strptime( input_start_date, 
+                                                "%Y.%m.%d" ).isoformat()
+    except ValueError:
+        try:
+            start_date = datetime.datetime.strptime( input_start_date, 
+                                                    "%Y-%m-%d" ).isoformat()
+        except ValueError:
+                start_date = datetime.datetime.strptime( input_start_date, 
+                                                        "%Y/%j" ).isoformat()
+    start_date = start_date + "Z"
     
     
+    if input_end_date is None:
+        end_date = "NOW"
+    else:
+        try:
+            end_date = datetime.datetime.strptime( input_end_date, 
+                                                    "%Y.%m.%d" ).isoformat()
+        except ValueError:
+            try:
+                end_date = datetime.datetime.strptime( input_end_date, 
+                                                    "%Y-%m-%d" ).isoformat()
+            except ValueError:
+                    end_date = datetime.datetime.strptime( input_end_date, 
+                                                        "%Y/%j" ).isoformat()
+        
+
     
-    ####xmlfile = "/tmp/Sentinel-download/query_results.xml"
-    ####img= parse_xml ( "/tmp/Sentinel-download/query_results.xml")
+    if len(location) == 2:
+        location_str='footprint:"Intersects(%f, %f)"' % ( location[0], location[1] )
+    elif len(location) == 4:
+        location_str = 'footprint:"Intersects( POLYGON(( " + \
+            "%f %f, %f %f, %f %f, %f %f, %f %f) ))"' % (  
+        location[0], location[0],
+        location[0], location[1],
+        location[1], location[1],
+        location[1], location[0],
+        location[0], location[0] )
     
-    ####tree = ET.ElementTree( file = xmlfile )
-    ##### Search for all the acquired images...
-    ####for elem in tree.iter (tag="{http://www.w3.org/2005/Atom}entry"):
-        ####for img in elem.getchildren():
-            ####try:
-                ####print ">>>",img.attrib['href']
-            ####except:
-                ####print img.attrib, ">>",img.text
+    time_str = 'beginposition:[%s TO %s]' % ( start_date, end_date )
+    
+    query = "%s AND %s AND %s" % ( location_str, time_str, sensor_str )
+    query_human = "%s%s" % ( hub_url, query  )
+    query = "%s%s" % ( hub_url, urllib2.quote(query ) )
+    print query_human
+    result = do_query ( query, api_username=username, api_password=password )
+    granules = parse_xml ( result )
+    
+    if not os.path.exists (output_dir):
+        os.mkdir ( output_dir )
+    ret_files = []
+    for granule in granules:
+        print granule['link'] +"$value"
+        download( (granule['link'] +"$value", os.path.join ( output_dir,
+                    granule['filename'].replace("SAFE", "zip") ) ) )
+        ret_files.append ( os.path.join ( output_dir,
+                    granule['filename'].replace("SAFE", "zip") ) ) 
+        
+    return granules, ret_files
+        
+if __name__ == "__main__":
+    location = (36.5333, 6.2833)
+    input_start_date = "2015.01.01"
+    input_end_date = None
+    
+    username = "guest"
+    password = "guest"
+    
+    input_sensor = "S2"
+    
+    output_dir = "/data/selene/ucfajlg/tmp/"
+    download_sentinel ( location, input_start_date, input_sensor, output_dir )
