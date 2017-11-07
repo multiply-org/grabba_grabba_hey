31ac35bee9c2af280bde1bcc6c1745e5c5f063d9
Jose Gomez-Dans <jgomezdans@gmail.com>
2017-11-07 16:39:48 +0000

    Polygon vertices where wrong. Fixes #11

--- grabba_grabba_hey/sentinel_downloader.py
+++ grabba_grabba_hey/sentinel_downloader.py
@@ -3,18 +3,20 @@
 A simple interface to download Sentinel-1 and Sentinel-2 datasets from
 the COPERNICUS Sentinel Hub.
 """
-from functools import partial
 import hashlib
-import os
 import datetime
-import sys
-import xml.etree.cElementTree as ET
+from functools import partial
+import logging
+import os
 import re
+import sys
+import time
+import xml.etree.cElementTree as ET
 
-import requests
 from concurrent import futures
 
-import logging
+import requests
+
 logging.basicConfig(level=logging.INFO)
 
 LOG = logging.getLogger(__name__)
@@ -100,7 +102,7 @@ def download_product(source, target, user="guest", passwd="guest"):
         md5_file = calculate_md5(target)
         if md5 == md5_file:
             return
-    chunks = 1048576 # 1MiB...
+    chunks = 1048576  # 1MiB...
     while True:
         LOG.debug("Getting %s" % source)
         r = requests.get(source, auth=(user, passwd), stream=True,
@@ -118,9 +120,8 @@ def download_product(source, target, user="guest", passwd="guest"):
                     cntr += 1
                     if cntr > 100:
                         dload += cntr * chunks
-                        LOG.info("\tWriting %d/%d [%5.2f %%]" % (dload, file_size,
-                                                              100. * float(dload) / 
-                                                              float(file_size)))
+                        LOG.info("\tWriting %d/%d [%5.2f %%]" % (
+                            dload, 100. * float(dload) / float(file_size)))
                         sys.stdout.flush()
                         cntr = 0
 
@@ -153,14 +154,14 @@ def parse_xml(xml):
         for img in elem.getchildren():
             if img.tag.find("id") >= 0:
                 granule['id'] = img.text
-            if img.tag.find("link") and img.attrib.has_key("href"):
+            if img.tag.find("link") and "href" in img.attrib:
 
                 if img.attrib['href'].find("Quicklook") >= 0:
                     granule['quicklook'] = img.attrib['href']
                 elif img.attrib['href'].find("$value") >= 0:
                     granule['link'] = img.attrib['href'].replace("$value", "")
 
-            if img.attrib.has_key("name"):
+            if "name" in img.attrib:
                 if img.attrib['name'] in fields_of_interest:
                     granule[img.attrib['name']] = img.text
 
@@ -184,7 +185,7 @@ def download_sentinel(location, input_start_date, input_sensor, output_dir,
         elif input_sensor.upper() == "S2":
             sensor = "Sentinel-2"
         sensor_str = 'platformname:%s' % sensor
-        #sensor_str = 'filename:%s' % input_sensor.upper()
+        # sensor_str = 'filename:%s' % input_sensor.upper()
     try:
         start_date = datetime.datetime.strptime(input_start_date,
                                                 "%Y.%m.%d").isoformat()
@@ -212,15 +213,16 @@ def download_sentinel(location, input_start_date, input_sensor, output_dir,
                                                       "%Y/%j").isoformat()
 
     if len(location) == 2:
-        location_str = 'footprint:"Intersects(%f, %f)"' % (location[0], location[1])
+        location_str = 'footprint:"Intersects(%f, %f)"' % (location[0],
+                                                           location[1])
     elif len(location) == 4:
-        location_str = 'footprint:"Intersects( POLYGON(( " + \
-            "%f %f, %f %f, %f %f, %f %f, %f %f) ))"' % (
-            location[0], location[0],
-            location[0], location[1],
-            location[1], location[1],
-            location[1], location[0],
-            location[0], location[0])
+        location_str = 'footprint:"Intersects( POLYGON((' + \
+            '%f %f, %f %f, %f %f, %f %f, %f %f) ))"' % (
+                location[0], location[1],
+                location[0], location[3],
+                location[2], location[3],
+                location[2], location[1],
+                location[0], location[1])
 
     time_str = 'beginposition:[%s TO %s]' % (start_date, end_date)
 
@@ -235,30 +237,31 @@ def download_sentinel(location, input_start_date, input_sensor, output_dir,
         os.mkdir(output_dir)
     ret_files = []
     for granule in granules:
-        download_product(granule['link'] + "$value", os.path.join(output_dir,
-                        granule['filename'].replace("SAFE", "zip")),
-                        user=username, passwd=password)
+        download_product(granule['link'] + "$value",
+                         os.path.join(output_dir, granule['filename'].replace(
+                             "SAFE", "zip")), user=username, passwd=password)
         ret_files.append(os.path.join(output_dir,
-                                      granule['filename'].replace("SAFE", "zip")))
+                                      granule['filename'].replace(
+                                          "SAFE", "zip")))
 
     return granules, ret_files
 
 
 def parse_aws_xml(xml_text, clouds=None):
-    
+
     tree = ET.ElementTree(ET.fromstring(xml_text))
     root = tree.getroot()
     files_to_get = []
     for elem in tree.iter():
         for k in elem.getchildren():
-            if k.tag.find ("Key") >= 0:
-                if k.text.find ("tiles") >= 0:
-                    files_to_get.append( k.text )
-                    
+            if k.tag.find("Key") >= 0:
+                if k.text.find("tiles") >= 0:
+                    files_to_get.append(k.text)
+
     if len(files_to_get) > 0 and clouds is not None:
-        
+
         for fich in files_to_get:
-            if fich.find ("metadata.xml") >= 0:
+            if fich.find("metadata.xml") >= 0:
                 metadata_file = aws_url_dload + fich
                 r = requests.get(metadata_file)
                 tree = ET.ElementTree(ET.fromstring(r.text))
@@ -269,14 +272,15 @@ def parse_aws_xml(xml_text, clouds=None):
                     else:
                         return files_to_get
     return files_to_get
-    
+
+
 def aws_grabber(url, output_dir):
     output_fname = os.path.join(output_dir, url.split("tiles/")[-1])
-    if not os.path.exists(os.path.dirname (output_fname)):
+    if not os.path.exists(os.path.dirname(output_fname)):
         # We should never get here, as the directory should always exist
         # Note that in parallel, this can sometimes create a race condition
         # Groan
-        os.makedirs (os.path.dirname(output_fname))
+        os.makedirs(os.path.dirname(output_fname))
     with open(output_fname, 'wb') as fp:
         while True:
             try:
@@ -321,45 +325,46 @@ def download_sentinel_amazon(start_date, output_dir,
         the_url = "{0}{1}".format(front_url, "/{0:d}/{1:d}/{2:d}/0/".format(
             this_date.year, this_date.month, this_date.day))
         r = requests.get(the_url)
-        more_files = parse_aws_xml (r.text, clouds=clouds)
-        
+        more_files = parse_aws_xml(r.text, clouds=clouds)
+
         if len(more_files) > 0:
             acqs_to_dload += 1
-            rqi = requests.get (the_url + "qi/")
-            raux = requests.get (the_url + "aux/")
-            qi = parse_aws_xml (rqi.text)
-            aux = parse_aws_xml (raux.text)
-            more_files.extend (qi)
-            more_files.extend (aux)
-            files_to_download.extend (more_files)
-            LOG.info("Will download data for %s..." % 
+            rqi = requests.get(the_url + "qi/")
+            raux = requests.get(the_url + "aux/")
+            qi = parse_aws_xml(rqi.text)
+            aux = parse_aws_xml(raux.text)
+            more_files.extend(qi)
+            more_files.extend(aux)
+            files_to_download.extend(more_files)
+            LOG.info("Will download data for %s..." %
                      this_date.strftime("%Y/%m/%d"))
-            
+
         this_date += one_day
     LOG.info("Will download %d acquisitions" % acqs_to_dload)
     the_urls = []
     if just_previews:
         the_files = []
         for fich in files_to_download:
-            if fich.find ("preview") >= 0:
-                the_files.append ( fich )
+            if fich.find("preview") >= 0:
+                the_files.append(fich)
         files_to_download = the_files
-        
+
     for fich in files_to_download:
         the_urls.append(aws_url_dload + fich)
-        ootput_dir = os.path.dirname ( os.path.join(output_dir,
-                                                    fich.split("tiles/")[-1]))
-        if not os.path.exists ( ootput_dir ):
-            
+        ootput_dir = os.path.dirname(os.path.join(output_dir,
+                                                  fich.split("tiles/")[-1]))
+        if not os.path.exists(ootput_dir):
+
             LOG.info("Creating output directory (%s)" % ootput_dir)
-            os.makedirs ( ootput_dir )
+            os.makedirs(ootput_dir)
     ok_files = []
-    LOG.info( "Downloading a grand total of %d files" % 
-            len ( files_to_download ))
+    LOG.info("Downloading a grand total of %d files" %
+             len(files_to_download))
     download_granule_patch = partial(aws_grabber, output_dir=output_dir)
     with futures.ThreadPoolExecutor(max_workers=n_threads) as executor:
         for fich in executor.map(download_granule_patch, the_urls):
             ok_files.append(fich)
+
 
 if __name__ == "__main__":    # location = (43.3650, -8.4100)
     # input_start_date = "2015.01.01"
@@ -377,7 +382,7 @@ if __name__ == "__main__":    # location = (43.3650, -8.4100)
     lng = -8.4100
     lat = 43.3650
     #lat = 39.0985 # Barrax
-    #lng = -2.1082 
+    #lng = -2.1082
     #lat = 28.55 # Libya 4
     #lng = 23.39
     print "Testing S2 on AWS..."
